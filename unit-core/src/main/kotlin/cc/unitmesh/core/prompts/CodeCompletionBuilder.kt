9e0e247b2b036ece678c695fa846215bf6efebcb
Phodal Huang <h@phodal.com>
2023-12-04 09:59:46 +0800

    feat: init basic arch code completion builder

---
+++ unit-core/src/main/kotlin/cc/unitmesh/core/prompts/CodeCompletionBuilder.kt
@@ -0,0 +1,9 @@
+package cc.unitmesh.core.prompts
+
+import cc.unitmesh.core.ContextBuilder
+
+class CodeCompletionBuilder: ContextBuilder {
+    override fun build(): String {
+        TODO("Not yet implemented")
+    }
+}
