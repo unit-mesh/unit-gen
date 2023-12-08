782411d3e28662ed44b3e3c6fa6f6b69a0292151
Phodal Huang <h@phodal.com>
2023-12-08 08:57:06 +0800

    refactor: extract for code

--- unit-picker/src/main/kotlin/cc/unitmesh/pick/prompt/builder/RelatedCodeCompletionBuilder.kt
+++ unit-picker/src/main/kotlin/cc/unitmesh/pick/prompt/builder/RelatedCodeCompletionBuilder.kt
@@ -5,23 +5,24 @@ import cc.unitmesh.pick.ext.toUml
 import cc.unitmesh.pick.prompt.Instruction
 import cc.unitmesh.pick.prompt.InstructionBuilder
 import cc.unitmesh.pick.prompt.InstructionContext
+import chapi.domain.core.CodeDataStruct
 
 class RelatedCodeCompletionBuilder(private val context: InstructionContext) : InstructionBuilder {
-    private var language: String = "";
+    private var language: String = ""
 
     override fun build(): List<Instruction> {
         language = context.job.fileSummary.language
         val container = context.job.container ?: return emptyList()
 
+        // 1. collection all related data structure by imports if exists in a file tree
         val relatedDataStructure = container.Imports.mapNotNull {
             context.fileTree[it.Source]?.container?.DataStructures
         }.flatten()
 
-        val relatedCode = relatedDataStructure.joinToString("\n") {
-            it.toUml()
-        }
+        // 2. convert all related data structure to uml
+        val relatedCode = relatedDataStructure.joinToString("\n", transform = CodeDataStruct::toUml)
 
-        return container.DataStructures.map { ds ->
+        val map = container.DataStructures.map { ds ->
             ds.Functions.map {
                 val position = it.Position
                 val beforeCursor = context.job.codeLines.subList(0, position.StartLine).joinToString("\n")
@@ -48,7 +49,11 @@ class RelatedCodeCompletionBuilder(private val context: InstructionContext) : In
                 |```""".trimMargin()
                 )
             }
-        }.flatten()
+        }
+
+        // convert completion for template to instruction
+
+        return map.flatten()
     }
 }
 
