37ad4f3fef10035e2925b7107fa893fcd8a8aca2
Phodal Huang <h@phodal.com>
2023-12-10 10:45:40 +0800

    fix: fix tests

--- code-quality/src/test/kotlin/cc/unitmesh/quality/JavaControllerAnalyserTest.kt
+++ code-quality/src/test/kotlin/cc/unitmesh/quality/JavaControllerAnalyserTest.kt
@@ -1,13 +1,13 @@
 package cc.unitmesh.quality;
 
-import cc.unitmesh.quality.extension.JavaRepositoryAnalyser
+import cc.unitmesh.quality.extension.JavaControllerAnalyser
 import chapi.domain.core.CodeDataStruct
 import kotlinx.serialization.json.Json
 import org.junit.jupiter.api.Test
 import java.io.File
 import kotlin.test.assertEquals
 
-class JavaRepositoryAnalyserTest {
+class JavaControllerAnalyserTest {
     private fun loadNodes(source: String): List<CodeDataStruct> {
         return Json { ignoreUnknownKeys = true }.decodeFromString(
             File(this.javaClass.classLoader.getResource(source)!!.file).readText()
@@ -16,7 +16,7 @@ class JavaRepositoryAnalyserTest {
     @Test
     fun `should return list of issues when checking API`() {
         val nodes = loadNodes("java/structs_HelloController.json")
-        val issues = JavaRepositoryAnalyser().analysis(nodes)
+        val issues = JavaControllerAnalyser().analysis(nodes)
 
         assertEquals(2, issues.size)
     }
