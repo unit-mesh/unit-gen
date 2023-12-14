4b7918aaeaece8136769e7fc8956adeffddab33c
Phodal Huang <h@phodal.com>
2023-12-14 11:12:07 +0800

    feat: add simliar chunk context

--- unit-core/src/main/kotlin/cc/unitmesh/core/intelli/SimilarChunkContext.kt
+++ unit-core/src/main/kotlin/cc/unitmesh/core/intelli/SimilarChunkContext.kt
@@ -2,8 +2,29 @@ package cc.unitmesh.core.intelli
 
 import cc.unitmesh.core.base.LLMCodeContext
 
-class SimilarChunkContext(val language: String, val paths: List<String>, val chunks: List<String>) : LLMCodeContext {
+class SimilarChunkContext(val language: String, val paths: List<String>?, val chunks: List<String>?) : LLMCodeContext {
     override fun format(): String {
-        TODO("Not yet implemented")
+        val commentPrefix = CommentService.getInstance().lineComment(language)
+
+        if (paths == null || chunks == null) return ""
+
+        val filteredPairs = paths.zip(chunks).filter { it.second.isNotEmpty() }
+
+        val queryBuilder = StringBuilder()
+        for ((path, chunk) in filteredPairs) {
+            val commentedCode = commentCode(chunk, commentPrefix)
+            queryBuilder.append("$commentPrefix Compare this snippet from $path:\n")
+            queryBuilder.append(commentedCode).append("\n")
+        }
+
+        return queryBuilder.toString().trim()
+    }
+
+    private fun commentCode(code: String, commentSymbol: String?): String {
+        if (commentSymbol == null) return code
+
+        return code.split("\n").joinToString("\n") {
+            "$commentSymbol $it"
+        }
     }
 }
