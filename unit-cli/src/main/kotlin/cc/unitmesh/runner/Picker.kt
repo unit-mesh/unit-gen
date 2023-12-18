46bf853fce1e540b56311761a0098acf48a95caf
Phodal Huang <h@phodal.com>
2023-12-18 10:39:24 +0800

    docs: update todos

--- unit-cli/src/main/kotlin/cc/unitmesh/runner/Picker.kt
+++ unit-cli/src/main/kotlin/cc/unitmesh/runner/Picker.kt
@@ -5,8 +5,12 @@ import cc.unitmesh.pick.SimpleCodePicker
 import cc.unitmesh.pick.prompt.Instruction
 import cc.unitmesh.runner.cli.ProcessorResult
 import cc.unitmesh.runner.cli.ProcessorUtils
+import cc.unitmesh.runner.cli.SourceCode
 import com.github.ajalt.clikt.core.CliktCommand
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
@@ -57,37 +61,37 @@ class PickerCommand : CliktCommand() {
     }
 }
 
-// Since the network is not stable, we need to retry
-//
-//    private fun extracted(projects: List<SourceCode>, outputDir: File) {
-//        runBlocking {
-//            val deferredResults = projects.map {
-//                async { processProject(it) }
-//            }
-//
-//            deferredResults.forEach { deferred ->
-//                val result = deferred.await()
-//                val outputFile = File(outputDir, result.repository.split("/").last() + ".json")
-//                val json = Json { prettyPrint = true }
-//                outputFile.writeText(json.encodeToString(result.content))
-//            }
-//        }
-//    }
-//
-//suspend fun processProject(code: SourceCode): ProcessorResult {
-//    return withContext(Dispatchers.Default) {
-//        logger.info("Start to process ${code.repository}")
-//        val pickerConfig = PickerConfig(
-//            url = code.repository,
-//            branch = code.branch,
-//            language = code.language
-//        )
-//
-//        val content = SimpleCodePicker(pickerConfig).execute()
-//        ProcessorResult(
-//            repository = code.repository,
-//            content = content
-//        )
-//    }
-//}
+// todo: spike better way to handle like this with Git clone limit ?
+private fun extracted(projects: List<SourceCode>, outputDir: File) {
+    runBlocking {
+        val deferredResults = projects.map {
+            async { processProject(it) }
+        }
+
+        deferredResults.forEach { deferred ->
+            val result = deferred.await()
+            val outputFile = File(outputDir, result.repository.split("/").last() + ".json")
+            val json = Json { prettyPrint = true }
+            outputFile.writeText(json.encodeToString(result.content))
+        }
+    }
+}
+
+suspend fun processProject(code: SourceCode): ProcessorResult {
+    return withContext(Dispatchers.Default) {
+        logger.info("Start to process ${code.repository}")
+        val pickerConfig = PickerOption(
+            url = code.repository,
+            branch = code.branch,
+            language = code.language
+        )
+
+        val content = SimpleCodePicker(pickerConfig).execute()
+        ProcessorResult(
+            repository = code.repository,
+            content = content,
+            outputName = pickerConfig.repoFileName()
+        )
+    }
+}
 
