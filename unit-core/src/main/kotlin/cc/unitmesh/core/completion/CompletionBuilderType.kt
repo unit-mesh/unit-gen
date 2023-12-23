5449f50542d3dd71ad107fbe4d76fb188fa6df29
Phodal Huang <h@phodal.com>
2023-12-23 19:48:53 +0800

    chore: add type for full class code

--- unit-core/src/main/kotlin/cc/unitmesh/core/completion/CompletionBuilderType.kt
+++ unit-core/src/main/kotlin/cc/unitmesh/core/completion/CompletionBuilderType.kt
@@ -1,8 +1,23 @@
 package cc.unitmesh.core.completion
 
 enum class CompletionBuilderType {
-    // TODO: support in future for this
+    /**
+     * generate code after cursor, like text after `Blog blog = `, will be `new Blog();`
+     */
     INLINE_COMPLETION,
+
+    /**
+     * generate the rest of code of correct block, like function inner code
+     */
     IN_BLOCK_COMPLETION,
+
+    /**
+     * generate code after block, like multiple functions
+     */
     AFTER_BLOCK_COMPLETION,
+
+    /**
+     * generate full file code, like test code, class code, api code
+     */
+    FULL_FILE_COMPLETION,
 }
