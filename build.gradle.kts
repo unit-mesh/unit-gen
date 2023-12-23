b900a9bd63b33e03e39c4ba1579776af3eaaf6df
Phodal Huang <h@phodal.com>
2023-12-23 22:41:09 +0800

    chore: release to 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ allprojects {
     }
 
     group = "cc.unitmesh"
-    version = "0.2.0-SNAPSHOT"
+    version = "0.2.0"
 
     java.sourceCompatibility = JavaVersion.VERSION_11
     java.targetCompatibility = JavaVersion.VERSION_11
