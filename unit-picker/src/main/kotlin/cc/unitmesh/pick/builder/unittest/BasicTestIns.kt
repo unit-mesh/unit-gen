18694af3b35c7cad40bf68f1d2fb824d3efac345
Phodal Huang <h@phodal.com>
2023-12-25 09:17:53 +0800

    refactor: extract to code

---
+++ unit-picker/src/main/kotlin/cc/unitmesh/pick/builder/unittest/BasicTestIns.kt
@@ -0,0 +1,51 @@
+package cc.unitmesh.pick.builder.unittest
+
+import cc.unitmesh.core.Instruction
+import cc.unitmesh.core.SupportedLang
+import cc.unitmesh.core.unittest.TestCodeBuilderType
+import cc.unitmesh.core.unittest.TypedTestIns
+
+class BasicTestIns(
+    val lang: SupportedLang,
+    val underTestCode: String,
+    val generatedCode: String,
+    val coreFrameworks: List<String> = listOf(),
+    val testFrameworks: List<String> = listOf(),
+    /**
+     * the Specification of the test
+     */
+    val specs: List<String> = listOf(),
+    private val relatedCode: List<String> = listOf(),
+    override val testType: TestCodeBuilderType,
+) : TypedTestIns() {
+    override fun unique(): Instruction {
+        val input = StringBuilder()
+
+        input.append(specs.joinToString("\n") { "- $it" })
+        input.append("\n")
+
+        if (coreFrameworks.isNotEmpty()) {
+            input.append("- You are working on a project that uses ${coreFrameworks.joinToString(", ")}\n")
+        }
+
+        if (testFrameworks.isNotEmpty()) {
+            input.append("- This project uses ${testFrameworks.joinToString(", ")} to test code.\n")
+        }
+
+        if (relatedCode.isNotEmpty()) {
+            input.append("Related code:\n")
+            input.append(relatedCode.joinToString("\n"))
+        }
+
+        input.append("Code under test:\n")
+        input.append("```${lang.name.lowercase()}\n")
+        input.append(underTestCode)
+        input.append("\n```")
+
+        return Instruction(
+            instruction = "Write unit test for following code.",
+            input = input.toString(),
+            output = generatedCode,
+        )
+    }
+}
