df2c429acfed2fa75f56b18d6b0197fa86a7be5b
Phodal Huang <h@phodal.com>
2023-12-27 11:15:53 +0800

    chore: init for python

---
+++ unit-picker/src/main/kotlin/cc/unitmesh/pick/worker/lang/PythonWorker.kt
@@ -0,0 +1,42 @@
+package cc.unitmesh.pick.worker.lang
+
+import cc.unitmesh.pick.ext.CodeDataStructUtil
+import cc.unitmesh.pick.ext.buildSourceCode
+import cc.unitmesh.pick.worker.WorkerContext
+import cc.unitmesh.pick.worker.base.LangWorker
+import cc.unitmesh.pick.worker.job.InstructionFileJob
+import chapi.ast.kotlinast.KotlinAnalyser
+import chapi.ast.typescriptast.TypeScriptAnalyser
+import org.slf4j.Logger
+
+class PythonWorker(override val context: WorkerContext) : LangWorker {
+    override val jobs: MutableList<InstructionFileJob> = mutableListOf()
+
+    override val fileTree: HashMap<String, InstructionFileJob> = hashMapOf()
+    override val logger: Logger = org.slf4j.LoggerFactory.getLogger(PythonWorker::class.java)
+
+    val extLength = ".pyt".length
+
+    override fun prepareJob(job: InstructionFileJob) {
+        this.jobs.add(job)
+
+        try {
+            val container = TypeScriptAnalyser().analysis(job.code, job.fileSummary.location)
+            job.codeLines = job.code.lines()
+            container.DataStructures.map { ds ->
+                ds.Imports = container.Imports
+
+                ds.Content = CodeDataStructUtil.contentByPosition(job.codeLines, ds.Position)
+                ds.Functions.map {
+                    it.apply {
+                        it.Content = CodeDataStructUtil.contentByPosition(job.codeLines, it.Position)
+                    }
+                }
+            }
+
+            job.container = container
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
