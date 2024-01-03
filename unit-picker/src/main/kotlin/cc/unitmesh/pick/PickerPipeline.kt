94d23ef7044c29c668b68a517b9736899244aad7
Phodal Huang <h@phodal.com>
2024-01-03 09:33:28 +0800

    chore: update Chapi to 2.2.1

---
+++ unit-picker/src/main/kotlin/cc/unitmesh/pick/PickerPipeline.kt
@@ -0,0 +1,8 @@
+package cc.unitmesh.pick
+
+/**
+ * Pipeline is a DSL for building a pipeline of operations for picking code.
+ */
+class PickerPipeline {
+    // repo -> filter -> threshold -> instruction
+}
